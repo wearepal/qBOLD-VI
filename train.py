65f1fcf5624e565cf8aa3a4ac0fbf74af309716b
Ashley McManamon <amcmanamon1@gmail.com>
2021-06-11 16:12:25 +0100

    Simpler representation for nll calculation

--- train.py
+++ train.py
@@ -18,8 +18,8 @@ def create_model():
 
 
 def loss_fn(y_true, y_pred):
-    oef_nll = -tf.math.log((1 / (y_pred[:, 1] / 2)) * tf.math.exp((-1 / 2) * ((y_true[:, 0] - y_pred[:, 0]) / (y_pred[:, 1] / 2))**2))
-    dbv_nll = -tf.math.log((1 / (y_pred[:, 3] / 2)) * tf.math.exp((-1 / 2) * ((y_true[:,1] - y_pred[:, 2]) / (y_pred[:, 3] / 2))**2))
+    oef_nll = -(-tf.math.log(y_pred[:, 1]/2) - (1/2)*((y_true[:, 0]-y_pred[:, 0])/tf.math.sqrt(y_pred[:, 1]))**2)
+    dbv_nll = -(-tf.math.log(y_pred[:, 3]/2) - (1/2)*((y_true[:, 1]-y_pred[:, 2])/tf.math.sqrt(y_pred[:, 3]))**2)
 
     nll = tf.add(oef_nll, dbv_nll)
 
