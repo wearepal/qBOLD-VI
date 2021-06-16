66e3bb159279ff5e31ea2e68b0c2a6692ff17c11
Ivor Simpson <ivor.simpson@gmail.com>
2021-06-16 15:43:06 +0100

    Added extra comment

--- signals.py
+++ signals.py
@@ -83,6 +83,7 @@ class SignalGenerationLayer(keras.layers.Layer):
         # The predicted signal should have the original shape with len(self.taus)
         signal = tf.reshape(signal, original_shape + (len(self._taus, )))
 
+        # Normalise the data based on where tau = 0 to remove arbitrary scaling and take the log
         signal = tf.math.log(signal/tf.expand_dims(signal[:, tf.where(self._taus == 0)[0][0]], 1))
 
         if self._simulate_noise:
