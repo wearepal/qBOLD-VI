117e4050e654aa5120578957304355c501fa3eab
Ivor Simpson <ivor.simpson@gmail.com>
2021-06-22 12:32:47 +0100

    Rewrote the original train.py to allow it to be convolutional or not.

--- train.py
+++ train.py
@@ -8,23 +8,38 @@ import argparse
 import configparser
 
 
-def create_model():
+def create_model(use_conv=False):
     model = keras.models.Sequential()
-    model.add(keras.layers.Dense(18, input_shape=(11,), activation='relu'))
-    for i in range(2):
-        model.add(keras.layers.Dense(18, activation='relu'))
-    # Removed sigmoid
+    if use_conv:
+        model.add(keras.layers.InputLayer(input_shape=(None, None, None, 11), ragged=False))
+    else:
+        model.add(keras.layers.InputLayer(input_shape=(11,)))
+
+    def create_layer(no_units):
+        if use_conv:
+            return keras.layers.Conv3D(no_units, kernel_size=(1, 1, 1), activation='relu')
+        else:
+            return keras.layers.Dense(no_units, activation='relu')
+
+    for i in range(3):
+        model.add(create_layer(18))
+
+    # Removed sigmoid from output
     model.add(keras.layers.Dense(4))
     return model
 
 
 def loss_fn(y_true, y_pred):
+    # Reshape the data such that we can work with either volumes or single voxels
+    y_true = tf.reshape(y_true, (-1, 2))
+    y_pred = tf.reshape(y_pred, (-1, 4))
+
     oef_mean = y_pred[:, 0]
     oef_log_std = y_pred[:, 1]
     dbv_mean = y_pred[:, 2]
     dbv_log_std = y_pred[:, 3]
-    oef_nll = -(-oef_log_std - (1/2)*((y_true[:, 0]-oef_mean)/tf.exp(oef_log_std))**2)
-    dbv_nll = -(-dbv_log_std - (1/2)*((y_true[:, 1]-dbv_mean)/tf.exp(dbv_log_std))**2)
+    oef_nll = -(-oef_log_std - (1 / 2) * ((y_true[:, 0] - oef_mean) / tf.exp(oef_log_std)) ** 2)
+    dbv_nll = -(-dbv_log_std - (1 / 2) * ((y_true[:, 1] - dbv_mean) / tf.exp(dbv_log_std)) ** 2)
 
     nll = tf.add(oef_nll, dbv_nll)
 
@@ -32,7 +47,6 @@ def loss_fn(y_true, y_pred):
 
 
 if __name__ == '__main__':
-
     config = configparser.ConfigParser()
     config.read('config')
     params = config['DEFAULT']
@@ -51,7 +65,12 @@ if __name__ == '__main__':
     x = np.genfromtxt(args.s, delimiter=',')
     y = np.genfromtxt(args.p, delimiter=',')
 
-    model = create_model()
+    train_conv = True
+    if train_conv:
+        x = np.reshape(x, (-1, 1, 1, 1, 11))
+        y = np.reshape(y, (-1, 1, 1, 1, 2))
+
+    model = create_model(train_conv)
     optimiser = tf.keras.optimizers.Adam()
 
     model.compile(optimiser, loss=loss_fn)
