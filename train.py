10cf6447ca3a6f63c5553a4aa8b23b953c9d7018
Ivor Simpson <ivor.simpson@gmail.com>
2021-06-22 16:52:19 +0100

    Added an initial version of the training pipeline that works for fine-tuning

--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 
 import tensorflow as tf
 from tensorflow import keras
+from signals import SignalGenerationLayer
 
 import numpy as np
 import argparse
@@ -15,17 +16,17 @@ def create_model(use_conv=False):
     else:
         model.add(keras.layers.InputLayer(input_shape=(11,)))
 
-    def create_layer(no_units):
+    def create_layer(no_units, activation='relu'):
         if use_conv:
-            return keras.layers.Conv3D(no_units, kernel_size=(1, 1, 1), activation='relu')
+            return keras.layers.Conv3D(no_units, kernel_size=(1, 1, 1), activation=activation)
         else:
-            return keras.layers.Dense(no_units, activation='relu')
+            return keras.layers.Dense(no_units, activation=activation)
 
     for i in range(3):
         model.add(create_layer(18))
 
     # Removed sigmoid from output
-    model.add(keras.layers.Dense(4))
+    model.add(create_layer(4, None))
     return model
 
 
@@ -46,21 +47,50 @@ def loss_fn(y_true, y_pred):
     return tf.reduce_mean(nll)
 
 
-def crop_3d():
-    crop_size = 20
+class ReparamTrickLayer(keras.layers.Layer):
+    def call(self, input, *args, **kwargs):
+        oef_sample = input[:, :, :, :, 0] + tf.random.normal(tf.shape(input[:, :, :, :, 0])) * tf.exp(
+            input[:, :, :, :, 1])
+        dbv_sample = input[:, :, :, :, 2] + tf.random.normal(tf.shape(input[:, :, :, :, 0])) * tf.exp(
+            input[:, :, :, :, 3])
 
-    train_x = np.zeros((x.shape[0], crop_size, crop_size, crop_size, x.shape[-1]))
-    train_y = np.zeros((y.shape[0], crop_size, crop_size, crop_size, y.shape[-1]))
+        samples = tf.stack([oef_sample, dbv_sample], -1)
+        samples = tf.abs(samples) + 1e-2
+        return samples
 
-    for i in range(x.shape[0]):
-        # Generate the starting point for croppings
-        crop = tf.random.uniform([3], 0, x.shape[1] - crop_size,
-                                 dtype=tf.int32)  # TODO: this will break when dimensions not equal
-        cropper = keras.layers.Cropping3D(
-            cropping=tuple(
-                (crop[i], x.shape[1] - (crop[i] + crop_size)) for i in range(3)))  # Produces crop_size shaped croppings
-        train_x[i] = cropper(tf.expand_dims(x[i], 0))
-        train_y[i] = cropper(tf.expand_dims(y[i], 0))
+
+def fine_tune_loss_fn(y_true, y_pred):
+    mask = y_true[:, :, :, :, -1]
+    residual = y_true[:, :, :, :, :-1] - y_pred
+    residual = tf.reduce_sum(tf.abs(residual), -1)
+    residual = tf.where(mask > 0, residual, tf.zeros_like(residual))
+
+    # keras.backend.print_tensor(residual)
+
+    return tf.reduce_sum(residual) / tf.reduce_sum(mask)
+
+
+def prepare_dataset(real_data):
+    real_dataset = tf.data.Dataset.from_tensor_slices(real_data)
+
+    def map_func(data):
+        data_shape = data.shape.as_list()
+        new_shape = data_shape[0:2] + [-1, ]
+        data = tf.reshape(data, new_shape)
+        data = tf.image.random_crop(value=data, size=(20, 20, data.shape[-1]))
+        data = tf.reshape(data, [20, 20] + data_shape[-2:])
+        mask = data[:, :, :, -1:]
+        # Normalise within the mask
+        data = tf.where(data[:, :, :, -1:] > 0, tf.math.log(data[:, :, :, :-1] / data[:, :, :, 2:3]),
+                        tf.zeros_like(data[:, :, :, :-1]))
+        # concat the mask
+        data = tf.concat([data, mask], -1)
+        return data[:, :, :, :-1], data
+
+    real_dataset = real_dataset.map(map_func)
+    real_dataset = real_dataset.batch(6, drop_remainder=True)
+    real_dataset = real_dataset.repeat(-1)
+    return real_dataset
 
 
 if __name__ == '__main__':
@@ -84,12 +114,31 @@ if __name__ == '__main__':
         x = np.reshape(x, (-1, 1, 1, 1, 11))
         y = np.reshape(y, (-1, 1, 1, 1, 2))
 
-    model = create_model(train_conv)
     optimiser = tf.keras.optimizers.Adam()
-
+    model = create_model(train_conv)
     model.compile(optimiser, loss=loss_fn)
 
     es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=1)
     mc = tf.keras.callbacks.ModelCheckpoint('model.h5', monitor='val_loss', verbose=1)
 
-    model.fit(x, y, epochs=500, callbacks=[es, mc], validation_split=0.2, batch_size=8)
+    model.fit(x, y, epochs=1, callbacks=[es, mc], validation_split=0.2, batch_size=8)
+
+    # Load real data for fine-tuning
+    real_data = np.load('/Users/is321/Documents/Data/qBold/hyperv_data/hyperv_ase.npy')
+    real_dataset = prepare_dataset(real_data)
+
+    valid_data = np.load('/Users/is321/Documents/Data/qBold/hyperv_data/baseline_ase.npy')
+    print(valid_data.shape)
+    valid_dataset = prepare_dataset(valid_data)
+
+
+    full_optimiser = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    full_model = keras.Sequential()
+    full_model.add(keras.layers.InputLayer((20, 20, 8, 11)))
+    full_model.add(model)
+    full_model.add(ReparamTrickLayer())
+    params['simulate_noise'] = 'False'
+    full_model.add(SignalGenerationLayer(params, False, True))
+
+    full_model.compile(full_optimiser, loss=fine_tune_loss_fn)
+    full_model.fit(real_dataset, validation_data=valid_dataset, steps_per_epoch=100, epochs=100, validation_steps=1)
