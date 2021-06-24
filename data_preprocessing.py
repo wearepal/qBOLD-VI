be1158a369fd9001c837e721fe740f104572a302
Ivor Simpson <ivor.simpson@gmail.com>
2021-06-24 13:30:04 +0100

    Added code for calculating SNR of original data (bit messy)

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,33 +2,66 @@
 # Purpose: Code for preparing real data for further analysis
 import nibabel as nib
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 data_dir = '/Users/is321/Documents/Data/qBold/hyperv_data/'
 
-subjects = ['CISC17352', 'CISC17543', 'CISC17987', 'CISC19890']
-
-img = nib.load(data_dir + subjects[3] + '/hyperv_ase.nii.gz')
-data = img.get_fdata()
 
 
-def estimate_noise_level(data):
+
+def estimate_noise_level():
+    import matplotlib.pyplot as plt
+
+    subjects = ['CISC17352', 'CISC17543', 'CISC17987', 'CISC19890']
+
+    dir_name = data_dir + subjects[2]
+    basename = 'baseline_ase'
+    brain_mask = dir_name + '/mask_' + basename + '_mask.nii.gz'
+    img = nib.load(dir_name + '/'+basename+'.nii.gz')
+    data = img.get_fdata()
+
+    mask = nib.load(brain_mask)
+    mask = mask.get_fdata()
+
+    mask = mask.reshape(-1)
+
+    within_mask_data = data.reshape(-1, 11)[mask>0, :]
+    outside_mask_data = data.reshape(-1, 11)[mask==0, :]
+
+    within_mask_mean = np.mean(within_mask_data, 0)
+    outside_mask_std = np.std(outside_mask_data, 0)
+
+
     # Estimate the SNR from the corner of the image (0 true signal)
-    corner = data[1:18, 1:18, :, :]
+    corner_vals = 12
+    corner = data[1:corner_vals, 1:corner_vals, :, :]
+    corner2 = data[-corner_vals:-1, -corner_vals:-1, :, :]
+    corner3 = data[-corner_vals:-1, 1:corner_vals, :, :]
+    corner4 = data[1:corner_vals, -corner_vals:-1, :, :]
+    corner = np.concatenate([corner, corner2, corner3, corner4], 0)
     # Reshape
     corner = corner.reshape(-1, 11)
+
+    corner_std = np.std(corner[:, :], 0)
+    snr = within_mask_mean / corner_std
+    norm_snr = snr / snr[3]
+    print(norm_snr, snr)
+    plt.hist(data[:,:,:,2].flatten())
+    #plt.show()
     # Normalise by tau =0 image and take the log
     corner = np.log(corner / corner[:, 2:3])
     # Log at the std-dev
     print(np.std(corner[:, 3:]))
     # plot the histogram for different taus
     plt.hist(corner)
-    plt.show()
+    #plt.show()
 
     # Look at the whole image histogram
     whole_image = data.reshape(-1, 11) / data.reshape(-1, 11)[:, 2:3]
     plt.hist(whole_image)
-    plt.show()
+    #plt.show()
+
+estimate_noise_level()
 
 
 def prepare_image(image_filename):
@@ -76,8 +109,8 @@ def prepare_data(directory, orig_filebasename):
         image_data = prepare_image(im_filename)
         data.append(image_data)
 
-    np.savez(directory+'/'+orig_filebasename, data)
+    np.save(directory+'/'+orig_filebasename, data)
 
 
-prepare_data(data_dir, "hyperv_ase")
+prepare_data(data_dir, "baseline_ase")
 #estimate_noise_level(data)
