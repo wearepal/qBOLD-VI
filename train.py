65aedcaacee47bc7e3d819eef8fd31ff3591bfa5
Ivor Simpson <ivor.simpson@gmail.com>
2021-06-24 16:44:30 +0100

    Model now fully supports fine-tuning, with extra convs a variational loss to make it similar to the synthetic results and a smoothness loss on the means.
    In need of some refactoring

--- train.py
+++ train.py
@@ -9,33 +9,73 @@ import argparse
 import configparser
 
 
-def create_model(use_conv=False):
-    model = keras.models.Sequential()
+def create_model(use_conv, system_constants=None):
     if use_conv:
-        model.add(keras.layers.InputLayer(input_shape=(None, None, None, 11), ragged=False))
+        input = keras.layers.Input(shape=(None, None, None, 11), ragged=False)
     else:
-        model.add(keras.layers.InputLayer(input_shape=(11,)))
+        input = keras.layers.Input(shape=(11,))
 
-    def create_layer(no_units, activation='relu'):
+    net = input
+
+    no_units = 20
+
+    if system_constants is not None:
+        const_net = keras.layers.Dense(no_units)(system_constants)
+
+    def create_layer(no_units, activation='gelu'):
         if use_conv:
             return keras.layers.Conv3D(no_units, kernel_size=(1, 1, 1), activation=activation)
         else:
             return keras.layers.Dense(no_units, activation=activation)
 
-    for i in range(3):
-        model.add(create_layer(18))
+    net = create_layer(no_units)(net)
+    if system_constants is not None and i == 0:
+        net = keras.layers.Multiply()([net, keras.layers.Reshape((1, 1, 1, -1))(const_net)])
 
-    # Removed sigmoid from output
-    model.add(create_layer(4, None))
-    return model
+    for i in range(1):
+        net = create_layer(no_units)(net)
+
+    net_penultimate = create_layer(no_units)(net)
+
+    if use_conv:
+        # Add a second output that uses 3x3x3 convs
+        second_net = keras.layers.Conv3D(no_units, kernel_size=(3, 3, 3), activation='gelu', padding='same')(net)
+        second_net = keras.layers.Conv3D(no_units, kernel_size=(3, 3, 3), activation='gelu', padding='same')(second_net)
+
+        # Add this to the penultimate output from the 1x1x1 network
+        second_net = keras.layers.Add()([second_net, net_penultimate])
+    else:
+        second_net = net_penultimate
+
+    # Create the final layer, which produces a mean and variance for OEF and DBV
+    final_layer = create_layer(4, activation=None)
+    # Create an output that just looks at individual voxels
+    output = final_layer(net_penultimate)
+    # Create another output that also looks at neighbourhoods
+    second_net = final_layer(second_net)
+
+    return keras.Model(inputs=[input], outputs=[output, second_net])
 
 
 def forward_transform(logit):
-    return tf.nn.sigmoid(logit)
+    oef, dbv = tf.split(logit, 2, -1)
+    oef = tf.nn.sigmoid(oef) * 0.8
+    dbv = tf.nn.sigmoid(dbv) * 0.3
+    output = tf.concat([oef, dbv], axis=-1)
+    return output
+
+
+def logit(signal):
+    # Inverse sigmoid function
+    return tf.math.log(signal / (1.0 - signal))
 
 
 def backwards_transform(signal):
-    return tf.math.log(signal/(1.0-signal))
+    oef, dbv = tf.split(signal, 2, -1)
+    oef = logit(oef / 0.8)
+    dbv = logit(dbv / 0.3)
+    output = tf.concat([oef, dbv], axis=-1)
+    return output
 
 
 def loss_fn(y_true, y_pred):
@@ -56,7 +96,35 @@ def loss_fn(y_true, y_pred):
     return tf.reduce_mean(nll)
 
 
+def oef_dbv_metrics(y_true, y_pred, oef):
+    """
+    Produce the MSE of the predictions of OEF or DBV
+    @param oef is a boolean, if False produces the output for DBV
+    """
+    # Reshape the data such that we can work with either volumes or single voxels
+    y_true = tf.reshape(y_true, (-1, 2))
+    y_pred = tf.reshape(y_pred, (-1, 4))
+    # keras.backend.print_tensor(tf.reduce_mean(tf.exp(y_pred[:,1])))
+    means = tf.stack([y_pred[:, 0], y_pred[:, 2]], -1)
+    means = forward_transform(means)
+    residual = means - y_true
+    if oef:
+        residual = residual[:, 0]
+    else:
+        residual = residual[:, 1]
+    return tf.reduce_mean(tf.square(residual))
+
+
+def oef_metric(y_true, y_pred):
+    return oef_dbv_metrics(y_true, y_pred, True)
+
+
+def dbv_metric(y_true, y_pred):
+    return oef_dbv_metrics(y_true, y_pred, False)
+
+
 class ReparamTrickLayer(keras.layers.Layer):
+    # Draw samples of OEF and DBV from the predicted distributions
     def call(self, input, *args, **kwargs):
         oef_sample = input[:, :, :, :, 0] + tf.random.normal(tf.shape(input[:, :, :, :, 0])) * tf.exp(
             input[:, :, :, :, 1])
@@ -64,7 +132,9 @@ class ReparamTrickLayer(keras.layers.Layer):
             input[:, :, :, :, 3])
 
         samples = tf.stack([oef_sample, dbv_sample], -1)
+        # Forward transform
         samples = forward_transform(samples)
+        # Clip to avoid really tiny/large values breaking the forward model with nans
         samples = tf.clip_by_value(samples, clip_value_min=1e-3, clip_value_max=0.99)
         return samples
 
@@ -72,44 +142,120 @@ class ReparamTrickLayer(keras.layers.Layer):
 def fine_tune_loss_fn(y_true, y_pred):
     mask = y_true[:, :, :, :, -1]
     residual = y_true[:, :, :, :, :-1] - y_pred
-    residual = tf.reduce_sum(tf.abs(residual), -1)
-    residual = tf.where(mask > 0, residual, tf.zeros_like(residual))
+    # As estimated from real data
+    std_dev = 0.08
+    loss = -(-std_dev - (1.0 / 2.0) * (residual / std_dev) ** 2)
 
-    # keras.backend.print_tensor(residual)
+    loss = tf.reduce_sum(loss, -1)
+    loss = tf.where(mask > 0, loss, tf.zeros_like(loss))
 
-    return tf.reduce_sum(residual) / tf.reduce_sum(mask)
+    return tf.reduce_sum(loss) / tf.reduce_sum(mask)
 
 
-def prepare_dataset(real_data):
-    real_dataset = tf.data.Dataset.from_tensor_slices(real_data)
+def kl_loss(true, predicted):
+    q_oef_mean, q_oef_log_std, q_dbv_mean, q_dbv_log_std = tf.split(predicted, 4, -1)
+    p_oef_mean, p_oef_log_std, p_dbv_mean, p_dbv_log_std, mask = tf.split(true, 5, -1)
 
-    def map_func(data):
+    def kl(q_mean, q_log_std, p_mean, p_log_std):
+        result = tf.exp(q_log_std * 2 - p_log_std * 2) + tf.square(p_mean - q_mean) * tf.exp(p_log_std * -2.0)
+        result = result + p_log_std * 2 - q_log_std * 2 - 1.0
+        return result * 0.5
+
+    kl_oef = kl(q_oef_mean, q_oef_log_std, p_oef_mean, p_oef_log_std)
+    kl_dbv = kl(q_dbv_mean, q_dbv_log_std, p_dbv_mean, p_dbv_log_std)
+    kl_op = (kl_oef + kl_dbv) * mask
+    kl_op = tf.where(mask > 0, kl_op, tf.zeros_like(kl_op))
+    # keras.backend.print_tensor(kl_op)
+    return tf.reduce_sum(kl_op) / tf.reduce_sum(mask)
+
+
+def get_constants(params):
+    # Put the system constants into an array
+    dchi = float(params['dchi'])
+    hct = float(params['hct'])
+    te = float(params['te'])
+    r2t = float(params['r2t'])
+    tr = float(params['tr'])
+    ti = float(params['ti'])
+    t1b = float(params['t1b'])
+    consts = np.array([dchi, hct, te, r2t, tr, ti, t1b], dtype=np.float32)
+    taus = tf.range(float(params['tau_start']), float(params['tau_end']),
+                    float(params['tau_step']), dtype=tf.float32)
+    consts = tf.concat([consts, taus], 0)
+    consts = tf.reshape(consts, (1, -1))
+    return consts
+
+
+def smoothness_loss(true_params, pred_params):
+    q_oef_mean, q_oef_log_std, q_dbv_mean, q_dbv_log_std = tf.split(pred_params, 4, -1)
+    pred_params = tf.concat([q_oef_mean, q_dbv_mean], -1)
+
+    diff_x = pred_params[:, :-1, :, :, :] - pred_params[:, 1:, :, :, :]
+    diff_y = pred_params[:, :, :-1, :, :] - pred_params[:, :, 1:, :, :]
+    diff_z = pred_params[:, :, :, :-1, :] - pred_params[:, :, :, 1:, :]
+
+    diffs = tf.reduce_mean(tf.abs(diff_x)) + tf.reduce_mean(tf.abs(diff_y)) + tf.reduce_mean(tf.abs(diff_z))
+    return diffs
+
+
+def prepare_dataset(real_data, model):
+    # Prepare the real data
+    real_data = np.float32(real_data)
+
+    normalised_data = tf.where(real_data[:, :, :, :, -1:] > 0,
+                               tf.math.log(real_data[:, :, :, :, :-1] / real_data[:, :, :, :, 2:3]),
+                               tf.zeros_like(real_data[:, :, :, :, :-1]))
+    predicted_distribution, _ = model.predict(normalised_data)
+
+    real_dataset = tf.data.Dataset.from_tensor_slices((real_data, predicted_distribution))
+
+    def map_func2(data, predicted_distribution):
         data_shape = data.shape.as_list()
         new_shape = data_shape[0:2] + [-1, ]
         data = tf.reshape(data, new_shape)
-        data = tf.image.random_crop(value=data, size=(20, 20, data.shape[-1]))
+
+        predicted_distribution_shape = predicted_distribution.shape.as_list()
+        predicted_distribution = tf.reshape(predicted_distribution, new_shape)
+
+        # concatenate to crop
+        crop_data = tf.concat([data, predicted_distribution], -1)
+        crop_data = tf.image.random_crop(value=crop_data, size=(20, 20, crop_data.shape[-1]))
+
+        # Separate out again
+        predicted_distribution = crop_data[:, :, -predicted_distribution.shape.as_list()[-1]:]
+        predicted_distribution = tf.reshape(predicted_distribution, [20, 20] + predicted_distribution_shape[-2:])
+
+        data = crop_data[:, :, :data.shape[-1]]
         data = tf.reshape(data, [20, 20] + data_shape[-2:])
         mask = data[:, :, :, -1:]
+
         # Normalise within the mask
         data = tf.where(data[:, :, :, -1:] > 0, tf.math.log(data[:, :, :, :-1] / data[:, :, :, 2:3]),
                         tf.zeros_like(data[:, :, :, :-1]))
         # concat the mask
         data = tf.concat([data, mask], -1)
-        return data[:, :, :, :-1], data
 
-    real_dataset = real_dataset.map(map_func)
+        predicted_distribution = tf.concat([predicted_distribution, mask], -1)
+
+        return data[:, :, :, :-1], {'predictions': predicted_distribution, 'predicted_images': data}
+
+    real_dataset = real_dataset.map(map_func2)
     real_dataset = real_dataset.batch(6, drop_remainder=True)
     real_dataset = real_dataset.repeat(-1)
     return real_dataset
 
-def save_predictions(model, data, filename):
+
+def save_predictions(model, data, filename, use_first_op=True):
     import nibabel as nib
 
-    data = tf.where(data[:, :, :, -1:] > 0, tf.math.log(data[:, :, :, :-1] / data[:, :, :, 2:3]),
-             tf.zeros_like(data[:, :, :, :-1]))
-    predictions = forward_transform(model.predict(data[:, :, :, :, :-1]))
-    images = np.split(predictions[:,:,:,:,:], data.shape[0], axis=0)
-    images = np.squeeze(np.concatenate(images, axis=-1),0)
+    data = tf.where(data[:, :, :, :, -1:] > 0, tf.math.log(data[:, :, :, :, :-1] / data[:, :, :, :, 2:3]),
+                    tf.zeros_like(data[:, :, :, :, :-1]))
+    predictions, predictions2 = model.predict(data)
+    if use_first_op is False:
+        predictions = predictions2
+    predictions = forward_transform(predictions)
+    images = np.split(predictions, data.shape[0], axis=0)
+    images = np.squeeze(np.concatenate(images, axis=-1), 0)
     affine = np.eye(4)
     array_img = nib.Nifti1Image(images, affine)
     nib.save(array_img, filename)
@@ -119,7 +265,6 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config')
     params = config['DEFAULT']
-
     parser = argparse.ArgumentParser(description='Train neural network for parameter estimation')
 
     parser.add_argument('-f', default='synthetic_data.npz', help='path to synthetic data file')
@@ -136,36 +281,52 @@ if __name__ == '__main__':
         x = np.reshape(x, (-1, 1, 1, 1, 11))
         y = np.reshape(y, (-1, 1, 1, 1, 2))
 
-    optimiser = tf.keras.optimizers.Adam()
+    train_x = x[:-1000, ...]
+    train_y = y[:-1000, ...]
+    valid_x = x[-1000:, ...]
+    valid_y = y[-1000:, ...]
+
+    optimiser = tf.keras.optimizers.Adam(learning_rate=1e-3)
+
+    # system_constants = get_constants(params)
     model = create_model(train_conv)
-    model.compile(optimiser, loss=loss_fn)
+    model.compile(optimiser, loss=[loss_fn, None], metrics=[[oef_metric, dbv_metric], None])
 
     es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=1)
     mc = tf.keras.callbacks.ModelCheckpoint('model.h5', monitor='val_loss', verbose=1)
+    synthetic_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
 
-    model.fit(x, y, epochs=30, callbacks=[es, mc], validation_split=0.2, batch_size=8)
+    synthetic_dataset = synthetic_dataset.shuffle(10000)
+    synthetic_dataset = synthetic_dataset.batch(128)
+
+    model.fit(synthetic_dataset, epochs=10, callbacks=[mc], validation_data=(valid_x, valid_y))
 
     # Load real data for fine-tuning
     real_data = np.load('/Users/is321/Documents/Data/qBold/hyperv_data/hyperv_ase.npy')
-    real_dataset = prepare_dataset(real_data)
+    real_dataset = prepare_dataset(real_data, model)
 
     valid_data = np.load('/Users/is321/Documents/Data/qBold/hyperv_data/baseline_ase.npy')
-    print(valid_data.shape)
-    valid_dataset = prepare_dataset(valid_data)
+
+    valid_dataset = prepare_dataset(valid_data, model)
 
     save_predictions(model, valid_data, 'after_pt')
 
-    full_optimiser = tf.keras.optimizers.Adam(learning_rate=1e-4)
-    full_model = keras.Sequential()
-    full_model.add(keras.layers.InputLayer((20, 20, 8, 11)))
-    full_model.add(model)
-    full_model.add(ReparamTrickLayer())
+    full_optimiser = tf.keras.optimizers.Adam(learning_rate=1e-5)
+    input_3d = keras.layers.Input((20, 20, 8, 11))
+    net = input_3d
+    _, predicted_distribution = model(net)
+
+    sampled_oef_dbv = ReparamTrickLayer()(predicted_distribution)
+
     params['simulate_noise'] = 'False'
-    full_model.add(SignalGenerationLayer(params, False, True))
-
-    full_model.compile(full_optimiser, loss=fine_tune_loss_fn)
-    full_model.fit(real_dataset, validation_data=valid_dataset, steps_per_epoch=100, epochs=10, validation_steps=1)
-    save_predictions(model, valid_data, 'after_ft')
-
-
-
+    output = SignalGenerationLayer(params, False, True)(sampled_oef_dbv)
+    full_model = keras.Model(inputs=[input_3d],
+                             outputs={'predictions': predicted_distribution, 'predicted_images': output})
+    def predictions_loss(t, p):
+        return kl_loss(t, p) + smoothness_loss(t, p)*10.0
+    full_model.compile(full_optimiser,
+                       loss={'predicted_images': fine_tune_loss_fn,
+                             'predictions': predictions_loss},
+                             metrics= {'predictions': [smoothness_loss, kl_loss]})
+    full_model.fit(valid_dataset, validation_data=valid_dataset, steps_per_epoch=100, epochs=20, validation_steps=1)
+    save_predictions(model, valid_data, 'after_ft', use_first_op=False)
