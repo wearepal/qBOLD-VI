865c8adb34a563b3472413c69b611ebf0830be88
Ivor Simpson <ivor.simpson@gmail.com>
2021-06-25 17:15:52 +0100

    Moved the data normalisation into the model. Lots of refactoring and adding of options. Currently seems to work pretty well and after fine-tuning creates quite smooth parameter maps

--- train.py
+++ train.py
@@ -9,7 +9,22 @@ import argparse
 import configparser
 
 
-def create_model(use_conv, system_constants=None):
+def create_model(use_conv=True, system_constants=None, no_units=18, use_layer_norm=False, dropout_rate=0.0):
+    def create_layer(_no_units, activation='gelu'):
+        if use_conv:
+            return keras.layers.Conv3D(_no_units, kernel_size=(1, 1, 1), activation=activation)
+        else:
+            return keras.layers.Dense(_no_units, activation=activation)
+
+    def normalise_data(_data):
+        orig_shape = tf.shape(_data)
+        _data = tf.reshape(_data, (-1, 11))
+        _data = tf.clip_by_value(_data, 1e-2, 1e8)
+        _data = _data / tf.reduce_mean(_data[:, 1:4], -1, keepdims=True)
+        _data = tf.math.log(_data)
+        _data = tf.reshape(_data, orig_shape)
+        return _data
+
     if use_conv:
         input = keras.layers.Input(shape=(None, None, None, 11), ragged=False)
     else:
@@ -17,30 +32,39 @@ def create_model(use_conv, system_constants=None):
 
     net = input
 
-    no_units = 20
+    net = keras.layers.Lambda(normalise_data)(net)
+
+    def add_normalizer(_net):
+        import tensorflow_addons as tfa
+        if dropout_rate > 0.0:
+            _net = keras.layers.Dropout(dropout_rate)(_net)
+        if use_layer_norm:
+            _net = tfa.layers.GroupNormalization(groups=1, axis=-1)(_net)
+        return _net
 
     if system_constants is not None:
         const_net = keras.layers.Dense(no_units)(system_constants)
 
-    def create_layer(no_units, activation='gelu'):
-        if use_conv:
-            return keras.layers.Conv3D(no_units, kernel_size=(1, 1, 1), activation=activation)
-        else:
-            return keras.layers.Dense(no_units, activation=activation)
-
     net = create_layer(no_units)(net)
+    net = add_normalizer(net)
     if system_constants is not None and i == 0:
         net = keras.layers.Multiply()([net, keras.layers.Reshape((1, 1, 1, -1))(const_net)])
 
     for i in range(1):
+        net = add_normalizer(net)
         net = create_layer(no_units)(net)
 
+    net = add_normalizer(net)
     net_penultimate = create_layer(no_units)(net)
 
     if use_conv:
         # Add a second output that uses 3x3x3 convs
-        second_net = keras.layers.Conv3D(no_units, kernel_size=(3, 3, 3), activation='gelu', padding='same')(net)
-        second_net = keras.layers.Conv3D(no_units, kernel_size=(3, 3, 3), activation='gelu', padding='same')(second_net)
+        ki = tf.keras.initializers.GlorotNormal()
+        second_net = keras.layers.Conv3D(no_units, kernel_size=(3, 3, 3), activation='gelu', padding='same',
+                                         kernel_initializer=ki)(net)
+        second_net = add_normalizer(second_net)
+        second_net = keras.layers.Conv3D(no_units, kernel_size=(3, 3, 3), activation='gelu', padding='same',
+                                         kernel_initializer=ki)(second_net)
 
         # Add this to the penultimate output from the 1x1x1 network
         second_net = keras.layers.Add()([second_net, net_penultimate])
@@ -139,17 +163,31 @@ class ReparamTrickLayer(keras.layers.Layer):
         return samples
 
 
-def fine_tune_loss_fn(y_true, y_pred):
-    mask = y_true[:, :, :, :, -1]
+def fine_tune_loss_fn(y_true, y_pred, student_t_df=None, sigma=0.08):
+    """
+    The std_dev of 0.08 is estimated from real data
+    """
+    import tensorflow_probability as tfp
+    mask = y_true[:, :, :, :, -1:]
+    y_true = y_true / (tf.reduce_mean(y_true[:, :, :, :, 1:4], -1, keepdims=True) + 1e-3)
+    y_pred = y_pred / (tf.reduce_mean(y_pred[:, :, :, :, 1:4], -1, keepdims=True) + 1e-3)
+    y_true = tf.where(mask > 0, tf.math.log(y_true), tf.zeros_like(y_true))
+    y_pred = tf.where(mask > 0, tf.math.log(y_pred), tf.zeros_like(y_pred))
+
     residual = y_true[:, :, :, :, :-1] - y_pred
-    # As estimated from real data
-    std_dev = 0.08
-    loss = -(-std_dev - (1.0 / 2.0) * (residual / std_dev) ** 2)
+    residual = tf.reshape(residual, (-1, 11))
+    mask = tf.reshape(mask, (-1, 1))
 
-    loss = tf.reduce_sum(loss, -1)
-    loss = tf.where(mask > 0, loss, tf.zeros_like(loss))
+    if student_t_df is not None:
+        dist = tfp.distributions.StudentT(df=student_t_df, loc=0.0, scale=sigma)
+        nll = -dist.log_prob(residual)
+    else:
+        nll = -(-sigma - np.sqrt(2.0 * np.pi) - (1.0 / 2.0) * (residual / sigma) ** 2)
 
-    return tf.reduce_sum(loss) / tf.reduce_sum(mask)
+    nll = tf.reduce_sum(nll, -1, keepdims=True)
+    nll = nll * mask
+
+    return tf.reduce_sum(nll) / tf.reduce_sum(mask)
 
 
 def kl_loss(true, predicted):
@@ -201,11 +239,8 @@ def smoothness_loss(true_params, pred_params):
 def prepare_dataset(real_data, model):
     # Prepare the real data
     real_data = np.float32(real_data)
-
-    normalised_data = tf.where(real_data[:, :, :, :, -1:] > 0,
-                               tf.math.log(real_data[:, :, :, :, :-1] / real_data[:, :, :, :, 2:3]),
-                               tf.zeros_like(real_data[:, :, :, :, :-1]))
-    predicted_distribution, _ = model.predict(normalised_data)
+    # Mask the data and make some predictions to provide a prior distribution
+    predicted_distribution, _ = model.predict(real_data[:, :, :, :, :-1] * real_data[:, :, :, :, -1:])
 
     real_dataset = tf.data.Dataset.from_tensor_slices((real_data, predicted_distribution))
 
@@ -229,9 +264,7 @@ def prepare_dataset(real_data, model):
         data = tf.reshape(data, [20, 20] + data_shape[-2:])
         mask = data[:, :, :, -1:]
 
-        # Normalise within the mask
-        data = tf.where(data[:, :, :, -1:] > 0, tf.math.log(data[:, :, :, :-1] / data[:, :, :, 2:3]),
-                        tf.zeros_like(data[:, :, :, :-1]))
+        data = data[:, :, :, :-1] * data[:, :, :, -1:]
         # concat the mask
         data = tf.concat([data, mask], -1)
 
@@ -248,9 +281,7 @@ def prepare_dataset(real_data, model):
 def save_predictions(model, data, filename, use_first_op=True):
     import nibabel as nib
 
-    data = tf.where(data[:, :, :, :, -1:] > 0, tf.math.log(data[:, :, :, :, :-1] / data[:, :, :, :, 2:3]),
-                    tf.zeros_like(data[:, :, :, :, :-1]))
-    predictions, predictions2 = model.predict(data)
+    predictions, predictions2 = model.predict(data[:, :, :, :, :-1] * data[:, :, :, :, -1:])
     if use_first_op is False:
         predictions = predictions2
     predictions = forward_transform(predictions)
@@ -271,35 +302,56 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    no_units = 20
+    kl_weight = 1.0
+    smoothness_weight = 5.0
+    # Switching to None will use a Gaussian error distribution
+    student_t_df = None
+    dropout_rate = 0.0
+    use_layer_norm = False
+    use_system_constants = False
+    no_pt_epochs = 20
+    no_ft_epochs = 20
+    pt_lr = 1e-3
+    ft_lr = 1e-4
+    im_loss_sigma = 0.08
+
     data_file = np.load(args.f)
     x = data_file['x']
     y = data_file['y']
 
     train_conv = True
-    # If we're building a convolutional model, reshape the synthetic data to look like single voxel images
+    # If we're building a convolutional model, reshape the synthetic data to look like images, note we only do
+    # 1x1x1 convs for pre-training
     if train_conv:
-        x = np.reshape(x, (-1, 1, 1, 1, 11))
-        y = np.reshape(y, (-1, 1, 1, 1, 2))
+        x = np.reshape(x, (-1, 10, 10, 5, 11))
+        y = np.reshape(y, (-1, 10, 10, 5, 2))
 
-    train_x = x[:-1000, ...]
-    train_y = y[:-1000, ...]
-    valid_x = x[-1000:, ...]
-    valid_y = y[-1000:, ...]
+    train_x = x[:-50, ...]
+    train_y = y[:-50, ...]
+    valid_x = x[-50:, ...]
+    valid_y = y[-50:, ...]
 
-    optimiser = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    synthetic_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
 
-    # system_constants = get_constants(params)
-    model = create_model(train_conv)
+    synthetic_dataset = synthetic_dataset.shuffle(10000)
+    synthetic_dataset = synthetic_dataset.batch(6)
+
+    optimiser = tf.keras.optimizers.Adam(learning_rate=pt_lr)
+
+    if use_system_constants:
+        system_constants = get_constants(params)
+    else:
+        system_constants = None
+
+    model = create_model(use_conv=train_conv, no_units=no_units, use_layer_norm=use_layer_norm,
+                         dropout_rate=dropout_rate, system_constants=system_constants)
     model.compile(optimiser, loss=[loss_fn, None], metrics=[[oef_metric, dbv_metric], None])
 
     es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=1)
     mc = tf.keras.callbacks.ModelCheckpoint('model.h5', monitor='val_loss', verbose=1)
-    synthetic_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
 
-    synthetic_dataset = synthetic_dataset.shuffle(10000)
-    synthetic_dataset = synthetic_dataset.batch(128)
-
-    model.fit(synthetic_dataset, epochs=10, callbacks=[mc], validation_data=(valid_x, valid_y))
+    model.fit(synthetic_dataset, epochs=no_pt_epochs, callbacks=[mc], validation_data=(valid_x, valid_y))
 
     # Load real data for fine-tuning
     real_data = np.load('/Users/is321/Documents/Data/qBold/hyperv_data/hyperv_ase.npy')
@@ -309,9 +361,10 @@ if __name__ == '__main__':
 
     valid_dataset = prepare_dataset(valid_data, model)
 
-    save_predictions(model, valid_data, 'after_pt')
+    save_predictions(model, valid_data, 'after_pt_baseline')
+    save_predictions(model, real_data, 'after_pt_hyperv')
 
-    full_optimiser = tf.keras.optimizers.Adam(learning_rate=1e-5)
+    full_optimiser = tf.keras.optimizers.Adam(learning_rate=ft_lr)
     input_3d = keras.layers.Input((20, 20, 8, 11))
     net = input_3d
     _, predicted_distribution = model(net)
@@ -322,11 +375,18 @@ if __name__ == '__main__':
     output = SignalGenerationLayer(params, False, True)(sampled_oef_dbv)
     full_model = keras.Model(inputs=[input_3d],
                              outputs={'predictions': predicted_distribution, 'predicted_images': output})
+
+
     def predictions_loss(t, p):
-        return kl_loss(t, p) + smoothness_loss(t, p)*10.0
+        return kl_loss(t, p) * kl_weight + smoothness_loss(t, p) * smoothness_weight
+
+
     full_model.compile(full_optimiser,
-                       loss={'predicted_images': fine_tune_loss_fn,
+                       loss={'predicted_images': lambda _x, _y: fine_tune_loss_fn(_x, _y, student_t_df=student_t_df,
+                                                                                  sigma=im_loss_sigma),
                              'predictions': predictions_loss},
-                             metrics= {'predictions': [smoothness_loss, kl_loss]})
-    full_model.fit(valid_dataset, validation_data=valid_dataset, steps_per_epoch=100, epochs=20, validation_steps=1)
-    save_predictions(model, valid_data, 'after_ft', use_first_op=False)
+                       metrics={'predictions': [smoothness_loss, kl_loss]})
+    full_model.fit(valid_dataset, validation_data=valid_dataset, steps_per_epoch=100, epochs=no_ft_epochs,
+                   validation_steps=1)
+    save_predictions(model, valid_data, 'after_ft_baseline', use_first_op=False)
+    save_predictions(model, real_data, 'after_ft_hyperv', use_first_op=False)
