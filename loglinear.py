83d8a5ff7a7e02fcc85d125dd17a84c10f96fbf9
Ashley McManamon <amcmanamon1@gmail.com>
2021-07-01 14:17:33 +0100

    Use the config params instead of hardcoded and cleaned code

--- loglinear.py
+++ loglinear.py
@@ -7,14 +7,6 @@ from sklearn.linear_model import LinearRegression
 import argparse
 import configparser
 
-tau = np.arange(-16, 65, 8) * 10 ** -3
-Hct = 0.34
-dChi0 = 0.264 * 10 ** -6
-gamma = 2.675 * 10 ** 4
-B0 = 3 * 10 ** 4
-phi = 1.34
-k = 0.03
-
 
 def save_predictions(predictions, filename):
     import nibabel as nib
@@ -30,31 +22,6 @@ def save_predictions(predictions, filename):
     save_im_data(predictions[:, :, :, :, 1:2], filename + '_dbv_ll')
 
 
-def ase_qbold_3d(ln_Sase):
-    ln_Sase[np.where(np.isnan(ln_Sase))] = 0
-    ln_Sase[np.where(np.isinf(ln_Sase))] = 0
-
-    Tc = 0.016
-
-    tau_lineID = np.where(tau > Tc)
-    w = 1 / tau[tau_lineID]
-    p = np.zeros((*ln_Sase.shape[:-1], 2))
-
-    for xID in tqdm(range(p.shape[0])):
-        for yID in range(p.shape[1]):
-            for zID in range(p.shape[2]):
-                X = tau[np.where(tau > 0.017)]
-                X = np.vstack((X, np.ones_like(X))).T
-                Y = np.squeeze(ln_Sase[xID, yID, zID, tau_lineID])
-
-                wls = LinearRegression()
-                wls.fit(X, Y, sample_weight=w)
-
-                p[xID, yID, zID] = [wls.coef_[0], wls.intercept_]
-
-    return p
-
-
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config')
@@ -65,21 +32,50 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    signals = np.load(args.f)[:1]
+    dchi = float(params['dchi'])
+    gamma = float(params['gamma'])
+    b0 = float(params['b0'])
+    hct = float(params['hct'])
+    te = float(params['te'])
+    r2t = float(params['r2t'])
+    tr = float(params['tr'])
+    ti = float(params['ti'])
+    t1b = float(params['t1b'])
+    taus = np.arange(float(params['tau_start']), float(params['tau_end']),
+                     float(params['tau_step']), dtype=np.float32)
+
+    signals = np.load(args.f)
+
     ln_Sase = np.log(signals)
+    ln_Sase[np.where(np.isnan(ln_Sase))] = 0
+    ln_Sase[np.where(np.isinf(ln_Sase))] = 0
 
     p = np.zeros((*signals.shape[:-1], 2))
 
-    for i in range(signals.shape[0]):
-        p[i] = ase_qbold_3d(ln_Sase[i])
+    for vID in range(signals.shape[0]):  # takes roughly 1 min per volume
 
-    s0_id = np.where(tau == 0)[0]
+        tau_lineID = np.where(taus > 0.016)
+        w = 1 / taus[tau_lineID]
+
+        for xID in tqdm(range(p.shape[1])):
+            for yID in range(p.shape[2]):
+                for zID in range(p.shape[3]):
+                    X = taus[tau_lineID]
+                    X = np.vstack((X, np.ones_like(X))).T
+                    Y = np.squeeze(ln_Sase[vID, xID, yID, zID, tau_lineID])
+
+                    wls = LinearRegression()
+                    wls.fit(X, Y, sample_weight=w)
+
+                    p[vID, xID, yID, zID] = [wls.coef_[0], wls.intercept_]
+
+    s0_id = np.where(taus == 0)[0]
 
     r2p = -p[:, :, :, :, 0:1]
     c = p[:, :, :, :, 1:]
 
     dbv = c - ln_Sase[:, :, :, :, s0_id]
-    oef = r2p / (dbv * gamma * (4/3) * np.pi * dChi0 * Hct * B0)
-    dhb = r2p / (dbv * gamma * (4/3) * np.pi * dChi0 * B0 * k)
+    oef = r2p / (dbv * gamma * (4 / 3) * np.pi * dchi * hct * b0)
+    dhb = r2p / (dbv * gamma * (4 / 3) * np.pi * dchi * b0 * 0.03)
 
     save_predictions(p, 'baseline')
