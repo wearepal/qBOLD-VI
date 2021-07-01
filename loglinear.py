f647748a914fd7ed591ff2aff9f376c1aac9c132
Ashley McManamon <amcmanamon1@gmail.com>
2021-07-01 14:06:01 +0100

    Used carbon python copy of existing matlab code to get working log linear model

--- loglinear.py
+++ loglinear.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
-import nibabel as nib
-import tensorflow as tf
+from tqdm import tqdm
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import argparse
 import configparser
+
+tau = np.arange(-16, 65, 8) * 10 ** -3
+Hct = 0.34
+dChi0 = 0.264 * 10 ** -6
+gamma = 2.675 * 10 ** 4
+B0 = 3 * 10 ** 4
+phi = 1.34
+k = 0.03
 
 
 def save_predictions(predictions, filename):
@@ -23,6 +30,31 @@ def save_predictions(predictions, filename):
     save_im_data(predictions[:, :, :, :, 1:2], filename + '_dbv_ll')
 
 
+def ase_qbold_3d(ln_Sase):
+    ln_Sase[np.where(np.isnan(ln_Sase))] = 0
+    ln_Sase[np.where(np.isinf(ln_Sase))] = 0
+
+    Tc = 0.016
+
+    tau_lineID = np.where(tau > Tc)
+    w = 1 / tau[tau_lineID]
+    p = np.zeros((*ln_Sase.shape[:-1], 2))
+
+    for xID in tqdm(range(p.shape[0])):
+        for yID in range(p.shape[1]):
+            for zID in range(p.shape[2]):
+                X = tau[np.where(tau > 0.017)]
+                X = np.vstack((X, np.ones_like(X))).T
+                Y = np.squeeze(ln_Sase[xID, yID, zID, tau_lineID])
+
+                wls = LinearRegression()
+                wls.fit(X, Y, sample_weight=w)
+
+                p[xID, yID, zID] = [wls.coef_[0], wls.intercept_]
+
+    return p
+
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config')
@@ -33,47 +65,21 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    dchi = float(params['dchi'])
-    gamma = float(params['gamma'])
-    b0 = float(params['b0'])
-    hct = float(params['hct'])
-    te = float(params['te'])
-    r2t = float(params['r2t'])
-    tr = float(params['tr'])
-    ti = float(params['ti'])
-    t1b = float(params['t1b'])
-    taus = np.arange(float(params['tau_start']), float(params['tau_end']),
-                     float(params['tau_step']), dtype=np.float32)
+    signals = np.load(args.f)[:1]
+    ln_Sase = np.log(signals)
 
-    signals = np.load(args.f)
+    p = np.zeros((*signals.shape[:-1], 2))
 
-    w = 1 / taus[np.where(taus > 0.016)]
-    x = taus[np.where(taus > 0.016)]
-    x = np.vstack((x, np.ones_like(x))).T
+    for i in range(signals.shape[0]):
+        p[i] = ase_qbold_3d(ln_Sase[i])
 
-    def map_fn(signal):
-        if signal[-1] == 0:
-            return 0, 0
+    s0_id = np.where(tau == 0)[0]
 
-        signal = signal[:-1]
-        signal /= signal[2]  # tried both with and without
-        signal = np.log(signal)  # tried both with and without
+    r2p = -p[:, :, :, :, 0:1]
+    c = p[:, :, :, :, 1:]
 
-        y = signal[np.where(taus > 0.016)]
-
-        wls = LinearRegression()
-        wls.fit(x, y, sample_weight=w)
-
-        m = wls.coef_[0]
-        c = wls.intercept_
-
-        r2p = -m
-        dbv = abs(c - signal[2])  # try with and without abs
-        oef = (3 * r2p) / (4 * np.pi * gamma * b0 * dchi * hct * dbv)
-
-        return np.array([oef, dbv])
-
-    p = np.apply_along_axis(map_fn, 4, signals)  # takes long time for all volumes try signals[0:1]
-    p = np.clip(p, a_min=1e-3, a_max=0.99)
+    dbv = c - ln_Sase[:, :, :, :, s0_id]
+    oef = r2p / (dbv * gamma * (4/3) * np.pi * dChi0 * Hct * B0)
+    dhb = r2p / (dbv * gamma * (4/3) * np.pi * dChi0 * B0 * k)
 
     save_predictions(p, 'baseline')
